9b729a82a6ed5f2e4db6970a7440f12e3244fa20
pupil <pupil@ivkhk.ee>
2021-09-08 09:36:27 +0300

    try push

--- src/mysecondpro/MySecondPro.java
+++ src/mysecondpro/MySecondPro.java
@@ -16,7 +16,7 @@ public class MySecondPro {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("My second programm");
+        System.out.println("My second programmыыыыыы");
         System.out.println("First changes");
         System.out.println("Second changes");
     }
